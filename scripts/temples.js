a24eafcd114412ed385670369e554a2fed3010b1
IsaacIkediashi <isaacikediashi192@gmail.com>
2026-09-08 18:48:11 +0100

    picture album

---
+++ scripts/temples.js
@@ -0,0 +1,20 @@
+const currentYear = document.querySelector('#currentyear');
+const lastModified = document.querySelector('#lastModified');
+
+const mainnav = document.querySelector('nav');
+const album = document.querySelector('#album');
+const hambutton = document.getElementById('menu');
+const header = document.querySelector('header');
+
+const dateTime = new Date();
+
+currentYear.textContent = dateTime.getFullYear();
+
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+hambutton.addEventListener('click', () => {
+mainnav.classList.toggle('show');
+hambutton.classList.toggle('show');
+album.classList.toggle('show');
+header.classList.toggle('show');
+});
--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,6 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list =  document.querySelector('ul');
 
-
-
-
-
 button.addEventListener('click', function(){
  if (input.value.trim() !== '') {
    const li = document.createElement('li');
